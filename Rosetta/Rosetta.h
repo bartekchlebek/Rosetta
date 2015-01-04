9346c1ea3b9d072f8fefc0697f860a60f5868459
Bartek Chlebek <bartek.public@gmail.com>
2015-01-05 00:16:30 +0100

    Initial Commit

---
+++ Rosetta/Rosetta.h
@@ -0,0 +1,19 @@
+//
+//  Rosetta.h
+//  Rosetta
+//
+//  Created by Bartek Chlebek on 05.01.2015.
+//  Copyright (c) 2015 Bartek Chlebek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Rosetta.
+FOUNDATION_EXPORT double RosettaVersionNumber;
+
+//! Project version string for Rosetta.
+FOUNDATION_EXPORT const unsigned char RosettaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Rosetta/PublicHeader.h>
+
+
---
+++ RosettaTests/RosettaTests.swift
@@ -0,0 +1,36 @@
+//
+//  RosettaTests.swift
+//  RosettaTests
+//
+//  Created by Bartek Chlebek on 05.01.2015.
+//  Copyright (c) 2015 Bartek Chlebek. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class RosettaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
