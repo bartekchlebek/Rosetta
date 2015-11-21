0d2d05cb43efd3a8d44857c8730682cc53e527ea
Bartek Chlebek <bartek.public@gmail.com>
2015-11-21 15:12:40 +0100

    Code cleanup

--- Rosetta/Rosetta.swift
+++ Rosetta/Rosetta.swift
@@ -63,7 +63,7 @@ public class Rosetta {
 				}
 			}
 
-			var success = !LogsContainError(logs)
+			let success = !LogsContainError(logs)
 			if success == true {
 				self.testRun = false
 				if let jsonDictionary = jsonDictionary {
@@ -198,58 +198,4 @@ public class Rosetta {
 		dictionary = nil
 		logs.removeAll(keepCapacity: false)
 	}
-}
-
-// ------------------------------------------------
-
-func valueForKeyPath(keyPath: [String], inDictionary dictionary: [String: AnyObject]) -> AnyObject? {
-	if keyPath.count == 0 {
-		return dictionary
-	}
-	else {
-		var branch = dictionary
-		for index in 0..<keyPath.count - 1 {
-			if let subdict = branch[keyPath[index]] as? [String: AnyObject] {
-				branch = subdict
-			}
-			else {
-				return nil
-			}
-		}
-		return branch[keyPath.last!]
-	}
-}
-
-func dictionaryBySettingValue(
-	value: AnyObject,
-	forKeyPath keyPath: [String],
-	inDictionary dictionary: [String: AnyObject]
-	) -> [String: AnyObject] {
-
-		if keyPath.count == 0 {
-			NSException(name: NSInternalInconsistencyException, reason: "ketPath must not be empty", userInfo: nil).raise()
-			abort()
-		}
-		else if keyPath.count == 1 {
-			var dictionary = dictionary
-			dictionary[keyPath.first!] = value
-			return dictionary
-		}
-		else {
-			var dictionary = dictionary
-			let firstKey = keyPath[0]
-			var keyPath = keyPath
-			keyPath.removeAtIndex(0)
-			if let subdict = dictionary[firstKey] as? [String: AnyObject] {
-				dictionary[firstKey] = dictionaryBySettingValue(value, forKeyPath: keyPath, inDictionary: subdict)
-			}
-			else {
-				dictionary[firstKey] = dictionaryBySettingValue(value, forKeyPath: keyPath, inDictionary: [:])
-			}
-			return dictionary
-		}
-}
-
-func setValue(value: AnyObject, atKeyPath keyPath: [String], inout inDictionary dictionary: [String: AnyObject]) {
-	dictionary = dictionaryBySettingValue(value, forKeyPath: keyPath, inDictionary: dictionary)
 }
