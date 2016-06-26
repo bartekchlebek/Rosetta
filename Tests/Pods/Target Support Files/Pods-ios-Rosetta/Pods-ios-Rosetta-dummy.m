ddb72a2c225b497bbef5832cdc58887321f090e8
Bartek Chlebek <bartek.chlebek@gmail.com>
2016-06-26 19:29:18 +0200

    Added post install script to Tests Podfile to set compiler to Swift 3
    https://github.com/CocoaPods/CocoaPods/issues/5521#issuecomment-226328062

--- Tests/Pods/Target Support Files/Pods-ios-Rosetta/Pods-ios-Rosetta-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ios_Rosetta : NSObject
-@end
-@implementation PodsDummy_Pods_ios_Rosetta
-@end
--- Tests/Pods/Target Support Files/Pods-osxTests-Rosetta/Pods-osxTests-Rosetta-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-
-FOUNDATION_EXPORT double RosettaVersionNumber;
-FOUNDATION_EXPORT const unsigned char RosettaVersionString[];
-
